94cff39c8c853c0daba65ac382096a7b933dd8ed
Stefano Calcaterra <calcaterrastefano3@gmail.com>
2019-11-10 14:25:32 +0100

    create product list

--- src/components/homePage/homePage.js
+++ src/components/homePage/homePage.js
@@ -1,12 +1,16 @@
 import React, { Component, Fragment } from 'react'
 import ProductsList from '../products/products'
+import Products from '../json/images'
 
 class HomeComponent extends Component{
 
     render(){
+
         return(
             <Fragment>
-                <ProductsList />
+                <ProductsList 
+                    products = {Products}
+                />
             </Fragment>
         );
     }
--- src/components/products/products.js
+++ src/components/products/products.js
@@ -1,5 +1,6 @@
+/* eslint-disable no-unused-expressions */
 import React, { Component, Fragment } from 'react'
-import jsonDataText from '../json/images.json'
+import Products from '../json/images.js'
 import {
     Card, CardImg, CardBody,
     CardTitle, CardSubtitle, Button
@@ -16,94 +17,25 @@ class ProductsList extends Component {
     }
   }
 
-  goToPageDetails = () => {
-
-  }
-
-  /*renderProduct = (json) => {
-    
-    /*json.forEach(i => {
-      this.setState({
-        dataProducts: i
-      })
-      console.log(i)
-    });
-
-    for(const i = 0 ; i <= json.length; i++){
-      return (
-        <Col md="3">
-          <Card>
-            <CardImg top width='100%' src={json[i].img} />
-            <CardBody>
-              <CardTitle>{json[i].nome}</CardTitle>
-              <CardSubtitle>{json[i].price}</CardSubtitle>
-              <Button>{json[i].textButton}</Button>
-            </CardBody>
-          </Card>
-        </Col>
-      )
-    }
-  };*/
-
-  /*console.log(i)
-      return (
-        <Col md="3">
-          <Card>
-            <CardImg top width='100%' src={i.img} />
-            <CardBody>
-              <CardTitle>{i.nome}</CardTitle>
-              <CardSubtitle>{i.price}</CardSubtitle>
-              <Button>{i.textButton}</Button>
-            </CardBody>
-          </Card>
-        </Col>
-  )*/
-
   render(){
-    const json = jsonDataText.products;
     return(
       <Fragment>
         <Row md="12" style={{width: '100%', margin: 0, padding: 0}}>
-          <Col md="3">
-            <Card>
-              <CardImg top width='100%' src={json[0].img} />
-              <CardBody>
-                <CardTitle>{json[0].nome}</CardTitle>
-                <CardSubtitle>{json[0].price}</CardSubtitle>
-                <Button>{json[0].textButton}</Button>
-              </CardBody>
-            </Card>
-          </Col>
-          <Col md="3">
-            <Card>
-              <CardImg top width='100%' src={json[1].img} />
-              <CardBody>
-                <CardTitle>{json[1].nome}</CardTitle>
-                <CardSubtitle>{json[1].price}</CardSubtitle>
-                <Button>{json[1].textButton}</Button>
-              </CardBody>
-            </Card>
-          </Col>
-          <Col md="3">
-            <Card>
-              <CardImg top width='100%' src={json[2].img} />
-              <CardBody>
-                <CardTitle>{json[2].nome}</CardTitle>
-                <CardSubtitle>{json[2].price}</CardSubtitle>
-                <Button>{json[2].textButton}</Button>
-              </CardBody>
-            </Card>
-          </Col>
-          <Col md="3">
-            <Card>
-              <CardImg top width='100%' src={json[3].img} />
-              <CardBody>
-                <CardTitle>{json[3].nome}</CardTitle>
-                <CardSubtitle>{json[3].price}</CardSubtitle>
-                <Button >{json[3].textButton}</Button>
-              </CardBody>
-            </Card>
-          </Col>
+          {this.props.products.map(item => {
+            console.log(item.nome)
+            return(
+              <Col md="3">
+                  <Card>
+                  <CardImg top width="100%" src={item.img} alt="Card image cap" />
+                  <CardBody>
+                    <CardTitle>{item.nome}</CardTitle>
+                    <CardSubtitle>{item.price}</CardSubtitle>
+                    <Button>{item.textButton}</Button>
+                  </CardBody>
+                </Card>
+              </Col>
+            )
+          })}
         </Row>
       </Fragment>
     );
